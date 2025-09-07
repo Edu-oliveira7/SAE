0c802bf02007f8595b4dae7237ca1f66b96a02b1
Edu-oliveira7 <aksedu0@gmail.com>
2025-09-06 23:18:35 -0300

    telas de registro sendo add

--- config/sae/urls.py
+++ config/sae/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('registrar/', views.registrar, name='registrar'),
+    path('entrar/', views.entrar, name='entrar'),
 ]
+
--- config/sae/views.py
+++ config/sae/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("<h1>SAE MVP Rodando 🚀</h1>")
+
+
+
+def registrar(request):
+    return render(request, 'registro/registre.html')
+
+def entrar(request):
+    return render(request, 'registro/entrar.html')
